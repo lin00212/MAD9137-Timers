187d0554a66ccf3f83c4e2149e9efaaaa1b926b9
Eason Lin <lin00212@algonquinlive.com>
2024-11-01 11:46:56 -0400

    Initial Commit

---
+++ IOSTimers/IOSTimersApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTimersApp.swift
+//  IOSTimers
+//
+//  Created by Eason Lin on 1/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOSTimersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
